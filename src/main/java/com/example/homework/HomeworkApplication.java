0839692802048795ddb8ffe8da4435f18e072db1
KoTaYava <146347467+KoTaYava@users.noreply.github.com>
2024-01-16 22:54:05 +0400

    home

--- src/main/java/com/example/homework/HomeworkApplication.java
+++ src/main/java/com/example/homework/HomeworkApplication.java
@@ -1,10 +1,7 @@
-package com.example.homework.demo;
+package com.example.homework;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @SpringBootApplication
 public class HomeworkApplication {
